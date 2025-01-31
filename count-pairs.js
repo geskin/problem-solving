7acf2da9d8847ab9da9101d49b39a863f700e6ff
Ehretz Geskin <sg3584@alum.barnard.edu>
2025-01-31 12:18:54 -0500

    Completing basic problems

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+// receives an array of integers and a number
+// finds the number of pairs of integers in the array whose sum is equal to the second parameter
+// assume no duplicate values in the array
+function countPairs(nums, target, left = 0, right = nums.length - 1, count = 0) {
+    if (right === 0) return count;
+
+    let sum = nums[left] + nums[right];
+
+    if (left === right) {
+        left = 0;
+        right--;
+        return countPairs(nums, target, left, right, count)
+    }
+
+    // check the sum of the right most number with each number to the left of it
+    if (sum === target) {
+        count++;
+        left++;
+        return countPairs(nums, target, left, right, count);
+    } else {
+        return countPairs(nums, target, left + 1, right, count);
+    }
+
 }
--- separate-positive.js
+++ separate-positive.js
@@ -3,5 +3,47 @@
  * does NOT make a copy of the input array
  * the numbers do NOT need to be sorted
  */
-function separatePositive() {
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    let pivVal = arr[start];
+    let pivIdx = start;
+
+    for (let i = start + 1; i <= end; i++) {
+        if (pivVal < arr[i]) {
+            pivIdx++;
+
+            [arr[i], arr[pivIdx]] = [arr[pivIdx], arr[i]];
+        }
+    }
+
+    [arr[start], arr[pivIdx]] = [arr[pivIdx], arr[start]];
+
+    return pivIdx
+}
+
+function separatePositive(arr, i = 0, pivIdx = pivot(arr)) { //
+    if (i === arr.length) return arr;
+
+    if (arr[i] > 0 && i > pivIdx) { //if the value is to the right of the pivIdx and greater than 0 swap those values
+        [arr[i], arr[pivIdx]] = [arr[pivIdx], arr[i]];
+        return separatePositive(arr, i + 1, pivIdx);
+    } else if (arr[i] < 0 && i < pivIdx) {
+        [arr[i], arr[pivIdx]] = [arr[pivIdx], arr[i]];
+        return separatePositive(arr, i + 1, pivIdx);
+    } else {
+        return separatePositive(arr, i + 1, pivIdx);
+    }
+
+    // let end = arr.length - 1;
+
+    // if (arr[i] < 0) {
+    //     [arr[i], arr[end]] = [arr[end], arr[i]];
+    //     return separatePositive(arr, i + 1);
+    // } else if (arr[i] > 0) {
+    //     [arr[0], arr[i]] = [arr[i], arr[0]];
+    //     return separatePositive(arr, i + 1);
+    // } else {
+    //     return separatePositive(arr, i + 1);
+    // }
+
 }
